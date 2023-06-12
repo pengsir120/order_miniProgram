40dd3480cc83b8cc85e699f16c497bfa9eb144a1
chenruipeng <chenruipeng@dongxinsoft.cn>
2023-06-12 17:53:58 +0800

    feat: project init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  version: "5.0.0",
+  note: '集成秒杀、砍价功能，以及一些优化',
+  subDomain: "23e26bd38af3e272a48d8d9f9266428d", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
+}
